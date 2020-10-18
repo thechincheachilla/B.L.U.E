d12e20fac56d355b5c3d226b690b2676f964784d
reidenChea <reidec@cs.washington.edu>
2020-10-17 19:50:22 -0700

    Major progress on all pages

---
+++ g.o.t.c.h.a/src/components/LegislatorSearch.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="mt-4">
+        <b-col>
+        <b-input-group>
+            <template v-slot:prepend>
+                <b-dropdown :text="legType" variant="success">
+                    <b-dropdown-item @click=updateLegHouse()>Representative</b-dropdown-item>
+                    <b-dropdown-item @click=updateLegSenate()>Senator</b-dropdown-item>
+                </b-dropdown>
+            </template>
+            <b-form-input></b-form-input>
+            <template v-slot:append>
+                <b-button @click=search() variant="info">Search</b-button>
+            </template>
+        </b-input-group>
+        </b-col>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LegislatorSearch",
+    data() {
+        return{
+            legType: "Legislator Type:"
+        }
+    },
+    methods: {
+        search() {
+            
+        },
+        updateLegHouse() {
+            this.legType = "Representative"
+        },
+        updateLegSenate() {
+            this.legType = "Senator"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- g.o.t.c.h.a/src/views/Legislation.vue
+++ g.o.t.c.h.a/src/views/Legislation.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
-        <p>Test</p>
+    <div id="legislation">
+        <h1 style="font-weight:bold">Legislation Lookup</h1>
+        <LegislationSearch />
     </div>
 </template>
 
 <script>
+import LegislationSearch from "../components/LegislationSearch.vue";
 export default {
     name: "Legislation",
+    components: {
+        LegislationSearch
+    }
 }
 </script>
+
+<style>
+#legislation {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
