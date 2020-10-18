a4761d742681e0ddecf254ce4946676375828e84
Huy Huynh <huy000@uw.edu>
2020-10-17 23:36:31 -0700

    Added legislator parser

--- API/legislator.py
+++ API/legislator.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import csv
+import json
 
 
 def parseLegislator(csvfile):
-    raw = pd.read_csv(csvfile)
-    print(raw)
+    df = pd.read_csv(csvfile).T.to_dict()
+    print(df[0].get("middle_name"))
+    
+    with open ('parsed_legislators.txt', 'w') as outfile:
+        json.dump(df, outfile)
+
 
 def main():
     parseLegislator("legislators-current.csv")
