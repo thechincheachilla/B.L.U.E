6ad3dd1a620c7d950a15d741f7d58a54c8fe89c5
reidenChea <reidec@cs.washington.edu>
2020-10-17 17:02:34 -0700

    Created home page

--- g.o.t.c.h.a/src/views/Home.vue
+++ g.o.t.c.h.a/src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home" class="home">
+    <h1 class="mb-4" style="font-weight:bold; font-size:50px">Project</h1>
+    <img alt="GOTCHA_Logo" src="../assets/GOTCHA_Logo.png" style="width:35%; height:35%" />
+    <h1 class="mt-4 mb-4" style="font-weight:bold;">No time to read the latest bills? We gotcha!</h1>
+    <b-container>
+      <div class="mt-3">
+        <b-card-group deck class="mb-3">
+          <b-card 
+            header="Lookup Legislators" 
+            class="text-center"
+          >
+            <b-card-text>
+              What have our legislators been up to? How does their voting record look? How can we contact them?
+              Use this search to find out!
+            </b-card-text>
+            <b-button variant="info">Search</b-button>
+          </b-card>
+          <b-card 
+            header="Lookup Legislation" 
+            class="text-center"
+          >
+            <b-card-text>
+              Want to find out what Congress has been up to? What bills are currently on the floor?
+              Click here to find out! 
+            </b-card-text>
+            <b-button variant="info">Search</b-button>
+          </b-card>
+        </b-card-group>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+
   }
 };
 </script>
+
+<style>
+#home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
